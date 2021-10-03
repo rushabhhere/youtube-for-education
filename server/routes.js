c7a345043339a89216b9ba61e2b56e0f80c322c4
Rushabh Javeri <javeri.rushabh45@gmail.com>
2021-10-03 18:17:53 +0530

    Changed image location, linked /about route

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const index_page = require('./controllers/home-controller');
 const search_page = require('./controllers/search-controller');
 const video_page = require('./controllers/video-controller');
+const about_page = require('./controllers/about-controller');
 
 router = express.Router();
 
@@ -10,5 +11,7 @@ router.get('/', index_page);
 router.get('/search', search_page);
 
 router.get('/watch', video_page);
+
+router.get('/about', about_page);
 
 module.exports = router;
