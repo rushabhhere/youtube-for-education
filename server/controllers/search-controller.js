64f3fef31c26cacd43aa1fa96e924c05c389493e
Rushabh Javeri <javeri.rushabh45@gmail.com>
2021-10-03 16:54:06 +0530

    Video page server side rendering

--- server/controllers/search-controller.js
+++ server/controllers/search-controller.js
@@ -16,38 +16,38 @@ const search_page = (request, response) => {
       resultType = 'playlist';
     }
 
-    const cachedResults = JSON.parse(
-      fs.readFileSync(path.resolve('./server/cached-results.json'), 'utf-8'),
-    );
+    // const cachedResults = JSON.parse(
+    //   fs.readFileSync(path.resolve('./server/cached-results.json'), 'utf-8'),
+    // );
 
-    if (cachedResults) {
-      response.render('search', {
-        query,
-        results: cachedResults,
-        resultType,
-        duration,
-        now: new Date(),
-      });
-    }
+    // if (cachedResults) {
+    //   response.render('search', {
+    //     query,
+    //     results: cachedResults,
+    //     resultType,
+    //     duration,
+    //     now: new Date(),
+    //   });
+    // }
 
-    // const endpoint = 'https://youtube.googleapis.com/youtube/v3/search';
-    // const maxResults = 10;
-    // const key = fs.readFileSync(path.resolve('./server/api-key.txt'), 'utf-8');
+    const endpoint = 'https://youtube.googleapis.com/youtube/v3/search';
+    const maxResults = 10;
+    const key = fs.readFileSync(path.resolve('./server/api-key.txt'), 'utf-8');
 
-    // const url = `${endpoint}?part=snippet&q=${query}&type=${resultType}&maxResults=${maxResults}&key=${key}`;
+    const url = `${endpoint}?part=snippet&q=${query}&type=${resultType}&maxResults=${maxResults}&key=${key}`;
 
-    // fetch(url)
-    //   .then(res => res.json())
-    //   .then(results => {
-    //     response.render('search', {
-    //       query,
-    //       results,
-    //       resultType,
-    //       duration,
-    //       now: new Date(),
-    //     });
-    //   })
-    //   .catch(err => console.error(err));
+    fetch(url)
+      .then(res => res.json())
+      .then(results => {
+        response.render('search', {
+          query,
+          results,
+          resultType,
+          duration,
+          now: new Date(),
+        });
+      })
+      .catch(err => console.error(err));
   } else {
     response.redirect('/');
   }
