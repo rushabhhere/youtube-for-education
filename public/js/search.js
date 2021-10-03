7b7ee9000a01e6c095012f858043b22dbf94bb5e
Rushabh Javeri <javeri.rushabh45@gmail.com>
2021-10-03 14:41:52 +0530

    Made progress on playlist results

--- public/js/search.js
+++ public/js/search.js
@@ -2,19 +2,24 @@ const videosButton = document.getElementById('videos');
 const playlistsButton = document.getElementById('playlists');
 
 let activeTab = 'videos';
+const url = new URL(window.location.href);
+
+if (url.searchParams.get('type') === 'playlist') {
+  activeTab = 'playlist';
+}
+
+console.log(activeTab);
 
 videosButton.addEventListener('click', () => {
-  if (activeTab === 'playlists') {
-    videosButton.classList.add('active-tab');
-    playlistsButton.classList.remove('active-tab');
-    activeTab = 'videos';
+  if (activeTab === 'playlist') {
+    url.searchParams.delete('type');
+    window.location.assign(url.toString());
   }
 });
 
 playlistsButton.addEventListener('click', () => {
   if (activeTab === 'videos') {
-    videosButton.classList.remove('active-tab');
-    playlistsButton.classList.add('active-tab');
-    activeTab = 'playlists';
+    url.searchParams.set('type', 'playlist');
+    window.location.assign(url.toString());
   }
 });
--- server/controllers/search-controller.js
+++ server/controllers/search-controller.js
@@ -1,25 +1,53 @@
 const path = require('path');
 const fs = require('fs');
 const fetch = require('node-fetch');
+const duration = require('duration-fns');
 
 const search_page = (request, response) => {
-  const { query } = request.query;
+  const { query, type } = request.query;
   if (query) {
     if (query.trim() === '') {
       response.redirect('/');
     }
 
-    const endpoint = 'https://youtube.googleapis.com/youtube/v3/search';
-    const type = 'video';
-    const maxResults = 10;
-    const key = fs.readFileSync(path.resolve('./server/api-key.txt'), 'utf-8');
+    let resultType = 'video';
 
-    const url = `${endpoint}?part=snippet&q=${query}&type=${type}&maxResults=${maxResults}&key=${key}`;
+    if (type === 'playlist') {
+      resultType = 'playlist';
+    }
 
-    fetch(url)
-      .then(res => res.json())
-      .then(results => response.render('search', { query, results }))
-      .catch(err => console.error(err));
+    const cachedResults = JSON.parse(
+      fs.readFileSync(path.resolve('./server/cached-results.json'), 'utf-8'),
+    );
+
+    if (cachedResults) {
+      response.render('search', {
+        query,
+        results: cachedResults,
+        resultType,
+        duration,
+        now: new Date(),
+      });
+    }
+
+    // const endpoint = 'https://youtube.googleapis.com/youtube/v3/search';
+    // const maxResults = 10;
+    // const key = fs.readFileSync(path.resolve('./server/api-key.txt'), 'utf-8');
+
+    // const url = `${endpoint}?part=snippet&q=${query}&type=${resultType}&maxResults=${maxResults}&key=${key}`;
+
+    // fetch(url)
+    //   .then(res => res.json())
+    //   .then(results => {
+    //     response.render('search', {
+    //       query,
+    //       results,
+    //       resultType,
+    //       duration,
+    //       now: new Date(),
+    //     });
+    //   })
+    //   .catch(err => console.error(err));
   } else {
     response.redirect('/');
   }
